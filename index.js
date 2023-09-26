69993e63ed24bd51d2f3fbce5fafc2ffeb6aa4f1
BrundaB-16 <142652357+BrundaB-16@users.noreply.github.com>
2023-09-26 21:32:45 +0530

    script.js

---
+++ index.js
@@ -0,0 +1,14 @@
+//==============nav===================
+var sidem=document.getElementById("sidemenu");
+
+        function openmenu(){
+            sidem.style.top="0";
+        }
+        function closemenu(){
+            sidem.style.top="-80%";
+        }
+
+
+
+
+        
